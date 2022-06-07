234be2b732a6f9e87e9395b5fc56ede9efee757a
JovanCY <97321359+JovanCY@users.noreply.github.com>
2022-06-06 23:28:09 -0700

    modularized the firebase key for testing

---
+++ public/firebaseInit.js
@@ -0,0 +1,12 @@
+// NOTE - INCLUDE YOUR FIREBASE CONFIG HERE FOR ANYTHING TO WORK:
+const firebaseConfig = {
+    apiKey: "AIzaSyCmXl8u8Jt5_UH08-5nENQNQEMXPGys9kk",
+    authDomain: "ghostvallis.firebaseapp.com",
+    databaseURL: "https://ghostvallis-default-rtdb.firebaseio.com",
+    projectId: "ghostvallis",
+    storageBucket: "ghostvallis.appspot.com",
+    messagingSenderId: "845637151347",
+    appId: "1:845637151347:web:ad179ef5e70c257ec31806",
+    measurementId: "G-L2XPD49Z8K"
+};
+firebase.initializeApp(firebaseConfig);
